379a86e5fb8b717bb21dd5fc3e1e6e2eeb5e19d0
12qqwd <jd116561@mail-ljcu.jcu.edu.au>
2025-05-29 14:38:01 +0800

    Add prac 1 files

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,27 @@
-"""
-Program to determine score status: invalid, bad, passable, or excellent.
-"""
+def get_score():
+    score = input("Enter score: ")
+    try:
+        score = float(score)
+        return score
+    except:
+        print("Invalid input")
+        return None
 
-score = float(input("Enter score: "))
+def check_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = get_score()
+    if score is not None:
+        result = check_score(score)
+        print(result)
+
+main()
+
