e8fabac5a9188b1d745a1fbc42f994bc9444798c
12qqwd <jd116561@mail-ljcu.jcu.edu.au>
2025-06-18 20:40:04 +0800

    Add prac 1 files

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,20 @@
+def main():
+    text = input("Text: ").strip().lower()
+    words = text.split()
+
+    word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+    sorted_words = sorted(word_counts.keys())
+    max_word_length = max(len(word) for word in sorted_words)
+
+    for word in sorted_words:
+        print(f"{word:{max_word_length}} : {word_counts[word]}")
+
+
+if __name__ == "__main__":
+    main()
